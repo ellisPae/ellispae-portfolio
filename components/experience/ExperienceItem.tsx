79ce57aecbd13a8df349139ec8cb3780e269b781
Ellis Pae <ellis.h.pae@gmail.com>
2026-01-02 18:14:54 -0500

    Remove comments for Experience Items

--- components/experience/ExperienceItem.tsx
+++ components/experience/ExperienceItem.tsx
@@ -8,11 +8,9 @@ const ExperienceItem = ({ experience }: ExperienceItemProps) => {
   const { role, company, date, bullets, tech } = experience;
 
   return (
-    <div className="relative flex gap-6 sm:gap-8">
-      {/* Dot */}
-      <div className="relative z-10 mt-2.5 h-2.5 w-2.5 rounded-full bg-neutral-900" />
+    <div className="relative flex gap-8">
+      <div className="relative z-10 mt-2 h-3 w-3 rounded-full bg-neutral-900" />
 
-      {/* Content */}
       <div className="flex-1">
         <h3 className="text-[0.98rem] sm:text-base font-semibold leading-snug">
           {role}
